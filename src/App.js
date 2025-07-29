c515fb90ba7612fea8112d4c5bb784d2cb76ca43
Denis Starkov <starkden1000@gmail.com>
2025-07-29 17:52:29 +0300

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import { useTelegram } from "./hooks/useTelegram";
 
 function App() {
-  const { onToggleButton, tg } = useTelegram;
+  const { onToggleButton, tg } = useTelegram();
 
   useEffect(() => {
     tg.ready();
