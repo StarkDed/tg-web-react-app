ddd880e58cfee1133e3c7fd8560c1e577d684815
Denis Starkov <starkden1000@gmail.com>
2025-07-29 17:30:11 +0300

    init

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import Button from "../Button.jsx";
+
+const Header = () => {
+  const tg = window.Telegram.WebApp;
+  const onClose = () => {
+    tg.close();
+  };
+
+  return (
+    <div className={"header"}>
+      <Button onClick={onClose}>Закрыть</Button>
+      <span className={"username"}>{tg.initDataUnsafe?.user?.username}</span>
+    </div>
+  );
+};
+
+export default Header;
